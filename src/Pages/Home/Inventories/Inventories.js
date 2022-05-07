226618199ce77b851b08e1031b835ca0a083adfe
Monirul-98 <monirulsifat@gmail.com>
2022-05-07 20:12:46 +0600

    Products added from database

--- src/Pages/Home/Inventories/Inventories.js
+++ src/Pages/Home/Inventories/Inventories.js
@@ -1,32 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import Inventory from '../Inventory/Inventory';
-import './Inventories.css';
+import React, { useEffect, useState } from "react";
+import Inventory from "../Inventory/Inventory";
+import "./Inventories.css";
 
 const Inventories = () => {
-    
-    const [inventories,setInventories] = useState([]);
-    useEffect( () => {
-        fetch('inventories.json')
-        .then(res => res.json())
-        .then(data => setInventories(data));
-    }, [])
+  const [inventories, setInventories] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setInventories(data));
+  }, []);
 
-
-    return (
-        <div className='container'>
-            <h1 className='mt-5 mb-5'>Our Inventories</h1>
-            <div className='inventories-container'>
-            {
-                inventories.map(inventory => <Inventory
-                key={inventory.id} 
-                inventory={inventory}
-                ></Inventory>)
-            }
-
-            </div>
-        </div>
-        
-    );
+  return (
+    <div className="container">
+      <h1 className="mt-5 mb-5">Our Inventories</h1>
+      <div className="inventories-container">
+        {inventories.map((inventory) => (
+          <Inventory key={inventory.id} inventory={inventory}></Inventory>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Inventories;
